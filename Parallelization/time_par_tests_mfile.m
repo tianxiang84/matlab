ab07e128f7c91909606c2b55dce18c6413a45c7c
tianxiang84 <tsu@slb.com>
2020-09-08 11:06:38 -0400

    Baseline model ready for testing.

---
+++ Parallelization/time_par_tests_mfile.m
@@ -0,0 +1,17 @@
+function status = time_par_tests_mfile(matrix_size, repeat_times)
+% time_par_tests_mfile: Perform a parallization test (baseline)
+
+M = repeat_times; %[-] how many times we repeat the operation
+N = matrix_size; %[-] matrix size
+
+seed = 42; % Specify the seed for MATLAB random number generator
+rng(seed);
+
+a = zeros(1,M); %[-] soring the results
+for i = 1:M
+    a(i) = max(abs(eig(rand(N))));
+end
+
+status = 0;
+
+end
